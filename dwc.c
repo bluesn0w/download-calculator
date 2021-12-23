1bea1f8edff9ccbceffd4b2c9f02b63a6339b1d3
bluesn0w <91155988+bluesn0w@users.noreply.github.com>
2021-12-23 20:28:19 +0100

    Add files via upload

---
+++ dwc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cstdlib>
+
+int main()
+{
+    float gbsize;
+    float time;
+    printf("Wie gross ist dein Download (in gb)\n");
+    scanf("%f", &gbsize);
+    printf("Wie schnell ist dein Internet ungefaehr (mb)\n");
+    time = gbsize * 1000.0 / 3.5 / 60.0 / 60.0;
+    int o1 = (int) time;
+    float f1 = (float) o1;
+    float f2 = (time - f1) ;
+    f2 = f2 * 60.0;
+    printf("Dein Download braucht%3.0fh und%3.0fmin\n", f1, f2);
+    system("pause");
+    return 0;
+}
+
